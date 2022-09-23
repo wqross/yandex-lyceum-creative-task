329fd76837ca1b9f456bcf813c757dadc391c5cc
Mikael Gevorgyan <92267859+wqross@users.noreply.github.com>
2022-09-23 15:33:11 +0300

    added header

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Typed JS
+let typeAdj = new Typed('#type_adj', {
+    strings: ['современный', 'крутой', 'креативный'],
+    smartBackspace: true,
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1300,
+    loop: true,
+});
