3092e80dda3a6afa02a65e977e0b12cbef7767c4
Mikael Gevorgyan <92267859+wqross@users.noreply.github.com>
2022-09-24 15:39:49 +0300

    added animation for skills on scroll

--- js/script.js
+++ js/script.js
@@ -7,3 +7,34 @@ let typeAdj = new Typed('#type_adj', {
     backDelay: 1300,
     loop: true,
 });
+
+// Animation on scroll
+
+const skillsFills = document.querySelectorAll('.skills__fill');
+const skillsProgressbars = document.querySelectorAll('.skills__progressbar');
+
+let scrollNow = window.scrollY;
+const scrollChange = 609;
+
+function animOnScroll() {
+    for (const skillsFill of skillsFills) {
+        skillsFill.style.animation = "tech-progress 2s 0.5s forwards";
+    }
+    for (const skillsProgressbar of skillsProgressbars) {
+        skillsProgressbar.style.animation = "soft-progress 2s 0.5s forwards";
+    }
+};
+
+window.addEventListener('scroll', () => { 
+    scrollNow = window.scrollY;
+    if (scrollNow >= scrollChange) { animOnScroll(); } 
+    else {
+        for (const skillsFill of skillsFills) {
+            skillsFill.style.animation = "";
+        }
+        for (const skillsProgressbar of skillsProgressbars) {
+            skillsProgressbar.style.animation = "";
+            skillsProgressbar.style.setProperty('--percentage', '0');
+        }
+    }
+});
